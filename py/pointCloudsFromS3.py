cc97b11fb026becde1e03ead24fb959e2324c5de
Joe Ferdinando <46873795+jgferdinando@users.noreply.github.com>
2022-06-30 11:08:45 -0400

    refactored

--- py/pointCloudsFromS3.py
+++ py/pointCloudsFromS3.py
@@ -25,10 +25,21 @@ def convertLatLon(lat,lon):
     x, y = transformer.transform(lat, lon)
     return x, y
 
+def getLazFile(lazfilename):
+    with laspy.open(lazfilename) as lz:
+        las = lz.read()
+        lidarPoints = np.array((las.X,las.Y,las.Z,las.intensity,las.classification, las.return_number, las.number_of_returns)).transpose()
+        lidarDF = pd.DataFrame(lidarPoints)
+        lidarDF.columns = ['X', 'Y', 'Z', 'intens', 'class', 'return_number', 'number_of_returns']
+        lidarDF['X'] = lidarDF['X'] * lz.header.scales[0] + lz.header.offsets[0]
+        lidarDF['Y'] = lidarDF['Y'] * lz.header.scales[1] + lz.header.offsets[1]
+        lidarDF['Z'] = lidarDF['Z'] * lz.header.scales[2] + lz.header.offsets[2]
+    return lidarDF
+
 #
 
 #can be copied from Google Maps...
-lat,lon = 40.70104756900311, -74.01311202477183
+lat,lon = 40.68589349531872, -73.98824172255344
 x,y = convertLatLon(lat,lon)
 
 s3 = boto3.resource('s3', config=Config(signature_version=UNSIGNED))
@@ -40,18 +51,11 @@ for obj in bucket.objects.filter(Prefix= prefix + 'ept.json'):
     key = obj.key
     body = obj.get()['Body']
     eptJson = json.load(body)
-    print(eptJson)
     span = eptJson['span']
-    xmin = eptJson['boundsConforming'][0]
-    ymin = eptJson['boundsConforming'][1]
-    zmin = eptJson['boundsConforming'][2]
-    xmax = eptJson['boundsConforming'][3]
-    ymax = eptJson['boundsConforming'][4]
-    zmax = eptJson['boundsConforming'][5]
+    [xmin,ymin,zmin,xmax,ymax,zmax] = eptJson['boundsConforming']
     
 locatorx = ( x - xmin ) / ( xmax - xmin ) 
 locatory = ( y - ymin ) / ( ymax - ymin )
-print(locatorx," ",locatory)
 
 try:
     os.mkdir('laz/')
@@ -60,23 +64,13 @@ except:
 
 # download highest level laz for entire extent
 lazfile = bucket.download_file(prefix + 'ept-data/0-0-0-0.laz','laz/0-0-0-0.laz')
-with laspy.open('laz/0-0-0-0.laz') as lz:
-    print(lz.header.scales)
-    las = lz.read()
-    lidarPoints = np.array((las.X,las.Y,las.Z,las.intensity,las.classification, las.return_number, las.number_of_returns)).transpose()
-    lidar_df = pd.DataFrame(lidarPoints)
-    lidar_df.columns = ['X', 'Y', 'Z', 'intens', 'class', 'return_number', 'number_of_returns']
-    lidar_df['X'] = lidar_df['X'] * lz.header.scales[0] + lz.header.offsets[0]
-    lidar_df['Y'] = lidar_df['Y'] * lz.header.scales[1] + lz.header.offsets[1]
-    lidar_df['Z'] = lidar_df['Z'] * lz.header.scales[2] + lz.header.offsets[2]
+lidar_df = getLazFile('laz/0-0-0-0.laz')
 
 print(lidar_df)
         
 for depth in range(1,11):
     binx = int( (locatorx * 2 ** ( depth ) ) // 1 ) 
     biny = int( (locatory * 2 ** ( depth ) ) // 1 ) 
-
-    print(binx,", ",biny,", ", 2 ** ( depth ))
 
     lazfile = prefix + 'ept-data/{}-{}-{}-'.format(depth,binx,biny)
     for obj in bucket.objects.filter(Prefix = lazfile ):
@@ -88,26 +82,15 @@ for depth in range(1,11):
             lazfile = bucket.download_file(prefix + 'ept-data/'+lazfilename,'laz/'+lazfilename)
         else: 
             pass
-        with laspy.open('laz/'+lazfilename) as lz:
-            las = lz.read()
-            lidarPoints = np.array((las.X,las.Y,las.Z,las.intensity,las.classification, las.return_number, las.number_of_returns)).transpose()
-            lidar_df2 = pd.DataFrame(lidarPoints)
-            lidar_df2.columns = ['X', 'Y', 'Z', 'intens', 'class', 'return_number', 'number_of_returns']
-            lidar_df2['X'] = lidar_df2['X'] * lz.header.scales[0] + lz.header.offsets[0]
-            lidar_df2['Y'] = lidar_df2['Y'] * lz.header.scales[1] + lz.header.offsets[1]
-            lidar_df2['Z'] = lidar_df2['Z'] * lz.header.scales[2] + lz.header.offsets[2]
+        lidar_df2 = getLazFile('laz/'+lazfilename)
         lidar_df = pd.concat([lidar_df,lidar_df2])
             
 print(lidar_df)
 
 lidar_df = lidar_df[lidar_df['Z'] > 0]
-lidar_df = lidar_df[lidar_df['Z'] < 100]
+lidar_df = lidar_df[lidar_df['Z'] < 30]
 
-fig = plt.figure(figsize=(12,12), dpi=300, constrained_layout=True)
-ax1 = fig.add_subplot()
-ax1.scatter(lidar_df['X'],lidar_df['Y'],marker="+",s=0.01,c=lidar_df['Z'],cmap='bone')
-
-boxSize = 100
+boxSize = 400
 
 lidar_df = lidar_df[lidar_df['X'] <= x + boxSize/2 ]
 lidar_df = lidar_df[lidar_df['X'] >= x - boxSize/2 ]
@@ -116,4 +99,8 @@ lidar_df = lidar_df[lidar_df['Y'] >= y - boxSize/2 ]
 
 print(lidar_df)
 
-
+fig = plt.figure(figsize=(12,12), dpi=300, constrained_layout=True)
+ax1 = fig.add_subplot(111, aspect='equal')
+ax1.scatter(lidar_df['X'],lidar_df['Y'],marker="+",s=0.01,c=lidar_df['Z'],cmap='bone')
+trees = lidar_df[lidar_df['number_of_returns'] - lidar_df['return_number'] > 0 ]
+ax1.scatter(trees['X'],trees['Y'],marker="+",s=0.01,c=trees['Z'],cmap='summer')
