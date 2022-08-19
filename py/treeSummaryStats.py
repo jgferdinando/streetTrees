00778bdf94a1b16ea1b906c3b4047e1095913015
Joe Ferdinando <46873795+jgferdinando@users.noreply.github.com>
2022-08-19 10:07:26 -0400

    Update treeSummaryStats.py

--- py/treeSummaryStats.py
+++ py/treeSummaryStats.py
@@ -12,8 +12,7 @@ import os
 import json
 import itertools
 
-def countDataFrame(treeFilePath):
-    treeJSONpath = 'shadeShadingShadedTrees/{}'.format(treeFilePath)
+def countDataFrame(treeJSONpath):
     if os.path.exists(treeJSONpath):
         with open(treeJSONpath) as f:
             treePoints = json.load(f)
@@ -45,24 +44,116 @@ for filename in os.listdir('shadeShadingShadedTrees/'):
 
 treedf = treedf[treedf['tree_id'].isin(treeids)]
 
-dates = ['2022_06_21','2022_08_07','2022_09_22','2022_11_07','2022_12_21']
-times = ['0800','0900','1000','1100','1200','1300','1400','1500','1600']
-conditions = ['inShade','shadingFacade','shadingGround']
+###############################################################################
 
-treedf['lidarTile'] = treedf.apply(lambda x: tileids[x['tree_id']] , axis=1)
+# dates = ['2022_06_21','2022_08_07','2022_09_22','2022_11_07','2022_12_21']
+# times = ['0800','0900','1000','1100','1200','1300','1400','1500','1600']
+# conditions = ['inShade','shadingFacade','shadingGround']
 
-print(treedf)
+# treedf['lidarTile'] = treedf.apply(lambda x: tileids[x['tree_id']] , axis=1)
 
-for date in dates:
-    for time in times:
-        for condition in conditions:           
-            treedf['{}_{}_{}'.format(date,time,condition)] = treedf.apply(lambda x: countDataFrame('{}_{}_{}_tile{}_{}.json'.format( x['tree_id'], date, time, x['lidarTile'], condition )) , axis=1)
+# print(treedf)
 
-for date in dates:
-    for time in times:
-        for condition in conditions:     
-            treedf['{}_{}_{}_proportion'.format(date,time,condition)] = treedf['{}_{}_{}'.format(date,time,condition)] / ( treedf['{}_{}_{}'.format(date,time,'inShade')] + 
-                                                                                                                          treedf['{}_{}_{}'.format(date,time,'shadingFacade')] + 
-                                                                                                                          treedf['{}_{}_{}'.format(date,time,'shadingGround')] )
+# for date in dates:
+#     for time in times:
+#         for condition in conditions:           
+#             treedf['{}_{}_{}'.format(date,time,condition)] = treedf.apply(lambda x: countDataFrame('shadeShadingShadedTrees/{}_{}_{}_tile{}_{}.json'.format( x['tree_id'], date, time, x['lidarTile'], condition )) , axis=1)
+
+# for date in dates:
+#     for time in times:
+#         for condition in conditions:     
+#             treedf['{}_{}_{}_proportion'.format(date,time,condition)] = treedf['{}_{}_{}'.format(date,time,condition)] / ( treedf['{}_{}_{}'.format(date,time,'inShade')] + 
+#                                                                                                                           treedf['{}_{}_{}'.format(date,time,'shadingFacade')] + 
+#                                                                                                                           treedf['{}_{}_{}'.format(date,time,'shadingGround')] )
             
-treedf.to_csv('treeShadeSummaryStats.csv')
+# treedf.to_csv('treeShadeSummaryStats.csv')
+
+###############################################################################
+
+summaryDictSchema = {
+    'treeid':[],
+    'lidarTile':[],
+    'year':[],
+    'month':[],
+    'day':[],
+    'hour':[],
+    'inShadeCount':[],
+    'shadingGroundCount':[],
+    'shadingBuildingCount':[],
+    'totalPointsCount':[],
+    'inShadeProportion':[],
+    'shadingGroundProportion':[],
+    'shadingBuildingProportion':[]
+    }
+
+summaryDF = pd.DataFrame.from_dict(summaryDictSchema)
+
+for filename in os.listdir('shadeShadingShadedTrees/'):
+    if filename.endswith('_shadingFacade.json'):
+        newLineSummaryDict = summaryDictSchema
+        
+        treeid = filename.split('_')[0]
+        year   = filename.split('_')[1]
+        month  = filename.split('_')[2]
+        day    = filename.split('_')[3]
+        hour   = filename.split('_')[4]
+        tile   = filename.split('_')[5].strip('tile')
+        
+        inshadefilename = 'shadeShadingShadedTrees/{}_{}_{}_{}_{}_tile{}_inShade.json'.format(treeid,year,month,day,hour,tile)
+        shadinggroundfilename = 'shadeShadingShadedTrees/{}_{}_{}_{}_{}_tile{}_shadingGround.json'.format(treeid,year,month,day,hour,tile)
+        shadingbuildingfilename = 'shadeShadingShadedTrees/{}_{}_{}_{}_{}_tile{}_shadingFacade.json'.format(treeid,year,month,day,hour,tile)
+        
+        inShadeCount = countDataFrame(inshadefilename)
+        shadingGroundCount = countDataFrame(shadinggroundfilename)
+        shadingBuildingCount = countDataFrame(shadingbuildingfilename)
+        
+        totalPointsCount = inShadeCount + shadingGroundCount + shadingBuildingCount
+        
+        if totalPointsCount > 0:
+            inShadeProportion = inShadeCount / totalPointsCount
+            shadingGroundProportion = shadingGroundCount / totalPointsCount
+            shadingBuildingProportion = shadingBuildingCount / totalPointsCount
+        else:
+            inShadeProportion = 0
+            shadingGroundProportion = 0
+            shadingBuildingProportion = 0
+        
+        
+        
+        newLineSummaryDict = {
+            'treeid':[treeid],
+            'lidarTile':[tile],
+            'year':[year],
+            'month':[month],
+            'day':[day],
+            'hour':[hour],
+            'inShadeCount':[inShadeCount],
+            'shadingGroundCount':[shadingGroundCount],
+            'shadingBuildingCount':[shadingBuildingCount],
+            'totalPointsCount':[totalPointsCount],
+            'inShadeProportion':[inShadeProportion],
+            'shadingGroundProportion':[shadingGroundProportion],
+            'shadingBuildingProportion':[shadingBuildingProportion]
+            }
+        
+        
+        newLineDF = pd.DataFrame.from_dict(newLineSummaryDict)
+        summaryDF = pd.concat([summaryDF,newLineDF])
+        summaryDF = summaryDF[[
+            'treeid',
+            'lidarTile',
+            'year',
+            'month',
+            'day',
+            'hour',
+            'inShadeCount',
+            'shadingGroundCount',
+            'shadingBuildingCount',
+            'totalPointsCount',
+            'inShadeProportion',
+            'shadingGroundProportion',
+            'shadingBuildingProportion'
+            ]]
+        
+        summaryDF.to_csv('treeShadeSummaryStatsV2.csv')
+        
